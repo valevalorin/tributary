29d3f9486639586f42eb6c0cc43d0645184753be
Duncan Kelly <duncan.kelly@perceptivesoftware.com>
2014-08-14 15:14:44 -0500

    Active track experiment

--- app.js
+++ app.js
@@ -3,6 +3,21 @@
     var app = angular.module('tributary', [ ]);
     app.controller('TribController', function(){
         this.currentPlayer = "yt";
-        this.queue = ["Some Song"];
+        track = {
+          title: "Watsky - All That I",
+          provider: "YT",
+          link: "https://youtube.com/?v=qpwoeuqpw",
+          active: false
+        };
+        this.queue = [];
+        for(var i = 0; i < 12; i++)
+        {
+            trackbuff = JSON.parse(JSON.stringify(track));
+            trackbuff.title = trackbuff.title + i;
+            console.log('added');
+            this.queue.push(trackbuff);
+        }
+        this.queue[0].active = true;
+
     });
 })();
